a80c4b12e66633e5b6e48c2563a13e455a58700d
Ajinkya Kamble <ch19b058@smail.iitm.ac.in>
2022-05-24 17:29:42 +0530

    changes

--- components/InsuranceClaims.js
+++ components/InsuranceClaims.js
@@ -1,7 +1,42 @@
+import InsuranceStyles from '../styles/InsuranceClaims.module.scss'
+
 const Insurance =()=>{
     return(
-        <p> Attempt Incoming</p>
-    )
+        <div className={InsuranceStyles.forDivMargin}>
+            
+            <h1 className={InsuranceStyles.font1}> INSURED<span className={InsuranceStyles.Red}>.</span></h1>
+            
+            <h2 className={InsuranceStyles.font2}> Is your Insurance Company  
+                denying your claims, 
+                paying less claims or
+                complicating the process</h2>
+
+            <h3 className={InsuranceStyles.font3}>
+                Let the
+                <span className={InsuranceStyles.Red}> experts </span>
+                handle your case!
+            </h3>
+           
+           <div className={InsuranceStyles.insChooser}>
+                <h1 className={InsuranceStyles.font4}> Choose the Insurance Type</h1>
+               
+                <button onClick={Clicked} className={InsuranceStyles.forButtons}>Health</button>
+                <button onClick={Clicked} className={InsuranceStyles.forButtons}>Life</button>
+                <button onClick={Clicked} className={InsuranceStyles.forButtons}>Vehicle</button>
+            
+            </div>
+            <form className={InsuranceStyles.forForm} action="/insurance-resolve-status" method="post">
+                <input className={InsuranceStyles.textBox} type="text" id="name" placeholder='Name'/>
+                <input type="text" id="contact" placeholder='Contact'/>
+                <button type="submit">Act</button>
+            </form>
+            
+        </div>
+        )
+}
+
+function Clicked(){
+
 }
 
 export default Insurance;
---
+++ pages/insurance-resolve-status.jsx
@@ -0,0 +1,9 @@
+import InsuranceStatus from '../components/InsuranceClaimStatus'
+
+const InsuranceResolveStatus = () =>{
+    return (
+        <InsuranceStatus/>
+    )
+}
+
+export default InsuranceResolveStatus
