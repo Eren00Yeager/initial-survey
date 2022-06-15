f6fb76e30a3965a215279b12c3dc53d43c94dd09
Ajinkya Kamble <ch19b058@smail.iitm.ac.in>
2022-06-15 17:27:30 +0530

    infiniteth change

--- components/InsuranceClaims.js
+++ components/InsuranceClaims.js
@@ -2,7 +2,7 @@ import InsuranceStyles from "../styles/InsuranceClaims.module.scss";
 import { Row, Container, Col, Form } from "react-bootstrap";
 import { useState, useRef } from "react";
 import Image from "next/image";
-import fail from "../pic/rejecc.png";
+import fail from "../pic/rejecc.svg";
 
 const Insurance = () => {
   const [type, setType] = useState("");
@@ -13,7 +13,7 @@ const Insurance = () => {
     e.preventDefault();
     console.log([type, name, contact]);
 
-    if(type==""){
+    if (type == "") {
       alert("Select the insurance type");
       return;
     }
@@ -43,12 +43,18 @@ const Insurance = () => {
     <Container className={InsuranceStyles.Container}>
       <br />
       <Row>
-        <p className={InsuranceStyles.font1}>Claim Remedy</p>
+        <Col></Col>
+        <Col lg="6">
+          <p className={InsuranceStyles.font1}>Claim Remedy</p>
+        </Col>
+        <Col className={InsuranceStyles.about1} style={{ textAlign: "center" }}>
+          <button className={InsuranceStyles.changeB}>About</button>
+        </Col>
       </Row>
-      <Row style={{ paddingTop: "0.5em" }}>
+      <Row style={{ paddingTop: "0.8em" }}>
         <Col></Col>
 
-        <Col xs="10" lg="8" className={InsuranceStyles.giveBorder}>
+        <Col xs="10" lg="9" className={InsuranceStyles.giveBorder}>
           <div className={InsuranceStyles.forDivflex}>
             <p className={InsuranceStyles.font2}>
               Claim <span style={{ color: "#ADADAD" }}>Rejected?</span>
@@ -60,186 +66,65 @@ const Insurance = () => {
             </p>
             <div className={InsuranceStyles.forSpImage}>
               {" "}
-              <Image src={fail} width="250%" height="250%" />
+              <Image src={fail} width="300%" height="300%" />
             </div>
           </div>
         </Col>
         <Col></Col>
       </Row>
-
-      <Row>
-        <Col lg="2" xs="1"></Col>
-        <Col lg="8" xs="10">
-          <div style={{ paddingTop: "0.8em" }}>
-            <p className={InsuranceStyles.font3}>
-              Let the
-              <span style={{ color: "#619698" }}> experts </span>
-              handle your case!
-            </p>
-          </div>
-        </Col>
-
-        <Col></Col>
-      </Row>
-
       <br />
-      <Row>
-        <Col lg="1" xs="1"></Col>
-        <Col lg="10" xs="10">
-          <Form className={InsuranceStyles.formouter} onSubmit={submitHandler}>
-            <Row>
-              <Col></Col>
-              <Col xs="10" lg="6">
-                <p className={InsuranceStyles.radiocontent}>
-                  Choose the Insurance type
-                </p>
-                <Row>
-                  <div class="btn-group" role="group">
-                    <Col>
-                      {" "}
-                      <input
-                        type="radio"
-                        class="btn-check"
-                        name="btnradio"
-                        id="btnradio1"
-                        autoComplete="off"
-                        value="Health"
-                        onClick={(e) => setType(e.target.value)}
-                      />
-                      <label
-                        class={`btn btn-primary ${InsuranceStyles.button}`}
-                        for="btnradio1"
-                      >
-                        Health
-                      </label>
-                    </Col>
-                    <Col>
-                      <input
-                        type="radio"
-                        class="btn-check"
-                        name="btnradio"
-                        id="btnradio2"
-                        autoComplete="off"
-                        value="Life"
-                        onClick={(e) => setType(e.target.value)}
-                      />
-                      <label
-                        class={`btn btn-primary ${InsuranceStyles.button}`}
-                        for="btnradio2"
-                      >
-                        Life
-                      </label>
-                    </Col>
-                    <Col>
-                      <input
-                        type="radio"
-                        class="btn-check"
-                        name="btnradio"
-                        id="btnradio3"
-                        autoComplete="off"
-                        value="Vehicle"
-                        onClick={(e) => setType(e.target.value)}
-                      />
-                      <label
-                        class={`btn  btn-primary  ${InsuranceStyles.button}`}
-                        for="btnradio3"
-                      >
-                        Vehicle
-                      </label>
-                    </Col>
-                  </div>
-                </Row>
-                {/*          
-            <Col>
-              <Button className={InsuranceStyles.button} name="Health" value="Health" onClick={buttonClick}>Health</Button>
+
+      <Row style={{ paddingTop: "1em" }}>
+        <Col></Col>
+        <Col lg="9">
+          <Row>
+            <Col lg="8">
+              <p
+                className={InsuranceStyles.font1}
+                style={{ textAlign: "left" }}
+              >
+                {" "}
+                Our experts can get your claim from any company!
+              </p>
+              <p className={InsuranceStyles.pforsomeText}>
+                We don’t charge you before we get your money!
+              </p>
+              <p className={InsuranceStyles.pforsomeText2}>So let’s get what
+                is yours!</p>
             </Col>
             <Col>
-              <Button className={InsuranceStyles.button} name="Life" value="Life" onClick={buttonClick}>Life</Button>
+              <p > 
+                asdasd adas dasd as nd asd asdn asda s ascas dasd asmnd n sad
+                asd asd snd asnd as
+              </p>
             </Col>
-            <Col>
-              <Button className={InsuranceStyles.button} name="Vehicle" value="Vehicle" onClick={buttonClick}>Vehicle</Button>
-            </Col> */}
-              </Col>
-              <Col></Col>
-            </Row>
-            <Row>
-              <div className={InsuranceStyles.forForm}>
-                <div className={InsuranceStyles.forInp}>
-                  {" "}
-                  <input
-                    className={InsuranceStyles.input}
-                    id="name"
-                    type="name"
-                    name="name"
-                    placeholder="Name"
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
+          </Row>
+        </Col>
 
-                <div className={InsuranceStyles.forInp}>
-                  <input
-                    className={InsuranceStyles.input}
-                    id="contact"
-                    type="contact"
-                    name="contact"
-                    placeholder="Contact"
-                    pattern="[0-9]{10}"
-                    required
-                    value={contact}
-                    onChange={(e) => setContact(e.target.value)}
-                  />
-                </div>
-                <div className={InsuranceStyles.forSub}>
-                  <button type="submit" className={InsuranceStyles.submit}>
-                    Act
-                  </button>
-                </div>
-              </div>
-            </Row>
-          </Form>
-        </Col>
         <Col></Col>
       </Row>
+
+      <br />
+      
       <br />
       <br />
-      <Row>
-        <Col></Col>
-        <Col className={InsuranceStyles.about} xs="10" lg="10">
-          <h1 className={InsuranceStyles.aboutHead}>About us</h1>{" "}
-          <p className={InsuranceStyles.aboutDesc}>
-            {" "}
-            We are engineers pained by the situation where People who pay
-            premiums for years have to struggle to get their claims. An
-            individual finds oneself helpless in front of the big companies and
-            their ever confusing terms and conditions.
-            <br />
-            <br />
-            Tired of this, we decided to change it forever through Claim Remedy.
-            At Claim Remedy, we provide individuals legal help to fight for
-            their claims. With our robust system, we believe we would succeed on
-            the misson.
-          </p>
-        </Col>
-        <Col></Col>
-      </Row>
+
       <br />
       <br />
       <Row className={InsuranceStyles.forFooter}>
         <Col></Col>
         <Col lg="8">
           <p className={InsuranceStyles.footerText}>
-            We are just a call away  @+91&nbsp;7297803114
+            We are just a call away @+91&nbsp;7297803114
           </p>
           <p className={InsuranceStyles.footerText2}>
-              or mail us at
-              <span style={{ color: "#1B45DB" }}>
-                {" "}
-                claimremedycares@gmail.com
-              </span>
-              <br />
-            </p>
+            or mail us at
+            <span style={{ color: "#1B45DB" }}>
+              {" "}
+              claimremedycares@gmail.com
+            </span>
+            <br />
+          </p>
           <p className={InsuranceStyles.footerText3}>
             ⓒ 2022 Claim Remedy &nbsp; FAQ&nbsp; &nbsp;About
           </p>
