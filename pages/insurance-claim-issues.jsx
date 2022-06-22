b6c57bb928bec4febc82063111776b7e4c7a0181
Ajinkya Kamble <ch19b058@smail.iitm.ac.in>
2022-06-22 11:27:52 +0530

    seo added

--- pages/insurance-claim-issues.jsx
+++ pages/insurance-claim-issues.jsx
@@ -8,6 +8,7 @@ const InsuranceClaimIssues = () =>{
         <title>Claim Remedy</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="google-site-verification" content="0lg-59aMd1ZNt5ueUJsMg8lansXlC0TaaJjQWNaPkDg"/>
       </Head>
 
         <Insurance/>
